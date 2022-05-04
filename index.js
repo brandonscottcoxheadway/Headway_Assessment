e0197465cae4f4bd59af5073b4b3b18d6d14a031
brandonscottcoxheadway <104935167+brandonscottcoxheadway@users.noreply.github.com>
2022-05-04 15:09:13 -0600

    Express and GraphQL

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "Test"
+      }
+    })
+  })
+});
+
+app.use(
+  "graphql",
+  expressGraphQL({
+    graphiql: true
+  })
+);
+
 const { ApolloServer, gql } = require("apollo-server");
 
 // Construct a schema, using GraphQL schema language
@@ -10,7 +33,7 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: (root, args, context) => "Hello world!"
+    books: () => books
   }
 };
 
@@ -22,3 +45,14 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin"
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster"
+  }
+];
